3fbdf8a38687e37f8b07ba3ea90f5b9572097145
BarrierTrio <vinylscratch270@gmail.com>
2024-06-01 03:37:45 -0700

    done

--- jokers/newjoker.lua
+++ jokers/newjoker.lua
@@ -1,5 +1,5 @@
 local jokerInfo = {
-	name = 'The NEW Joker! [WIP]',
+	name = 'The NEW Joker!',
 	config = {},
 	text = {
 		"Played cards with an",
@@ -12,18 +12,34 @@ local jokerInfo = {
 	canEternal = true
 }
 
---[[
+
 function jokerInfo.locDef(self)
-	return { G.GAME.probabilities.normal }
+	return {self.ability.extra.mult}
 end
 
+
+function jokerInfo.init(self)
+	self.ability.extra = {
+		mult = 4
+	}
+end
+
+--[[
 function jokerInfo.init(self)
 
 end
 ]]--
 
 function jokerInfo.calculate(self, context)
-	--todo
+	if context.individual and context.cardarea == G.play and not self.debuff then
+		if context.other_card.ability.effect ~= 'Base'
+		then
+			return {
+				mult = self.ability.extra.mult,
+				card = self
+			}
+		end
+	end
 end
 
 
