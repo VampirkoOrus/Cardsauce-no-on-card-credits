cad188ac617abab47e9e275d45d8c3b0ee7d999f
BarrierTrio <vinylscratch270@gmail.com>
2024-05-31 23:52:10 -0700

    card works, still need to fix transformation timing

--- jokers/twoface.lua
+++ jokers/twoface.lua
@@ -22,8 +22,35 @@ function jokerInfo.init(self)
 end
 ]]--
 
+
+--card works, just need to fix timing on the card changing
 function jokerInfo.calculate(self, context)
-	--todo
+	if context.cardarea == G.jokers and context.before and not self.debuff and not context.blueprint then
+		for k, v in ipairs(context.full_hand) do 
+			if v:get_id() == 14 then 
+				local suit_prefix = string.sub(v.base.suit, 1, 1)..'_'
+				v:set_base(G.P_CARDS[suit_prefix..2])
+				G.E_MANAGER:add_event(Event({
+					func = function()
+						v:juice_up()
+						return true
+					end
+				})) 
+				card_eval_status_text(self, 'extra', nil, nil, nil, {message = "Twoed!", colour = G.C.MONEY})
+
+			elseif v:get_id() == 2 then 
+				local suit_prefix = string.sub(v.base.suit, 1, 1)..'_'
+				v:set_base(G.P_CARDS[suit_prefix..'A'])
+				G.E_MANAGER:add_event(Event({
+					func = function()
+						v:juice_up()
+						return true
+					end
+				})) 
+				card_eval_status_text(self, 'extra', nil, nil, nil, {message = "Aced!", colour = G.C.MONEY})
+			end
+		end
+	end
 end
 
 
