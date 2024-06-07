16cd1add55daa9abff15bd7c6f5884dba786390c
BarrierTrio <vinylscratch270@gmail.com>
2024-06-07 02:20:07 -0700

    easter egg effect

--- jokers/chad.lua
+++ jokers/chad.lua
@@ -22,14 +22,26 @@ local jokerInfo = {
 function jokerInfo.locDef(self)
 	return { G.GAME.probabilities.normal }
 end
-
-function jokerInfo.init(self)
-
-end
 ]]--
+function jokerInfo.init(self)
+	self.ability.extra = 2
+end
 
 function jokerInfo.calculate(self, context)
-	--todo
+	if context.other_joker and not self.debuff and not context.blueprint then
+		if (context.other_joker.config.center.name == ('No No No No No No No No No No No') or context.other_joker.ability.name == 'Hanging Chad' or context.other_joker.ability.name == 'Showman') and self ~= context.other_joker then
+			G.E_MANAGER:add_event(Event({
+				func = function()
+					context.other_joker:juice_up(0.5, 0.5)
+					return true
+				end
+			})) 
+			return {
+				message = localize{type='variable',key='a_xmult',vars={self.ability.extra}},
+            	Xmult_mod = self.ability.extra
+			}
+		end
+	end
 end
 
 
