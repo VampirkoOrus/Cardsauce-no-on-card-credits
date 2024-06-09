916e36db426fb957dbdba3942f4120d491641779
BarrierTrio <vinylscratch270@gmail.com>
2024-06-09 02:42:33 -0700

    done

--- jokers/pacman.lua
+++ jokers/pacman.lua
@@ -4,7 +4,7 @@ local jokerInfo = {
 	text = {
 		"This Joker gains {C:mult}+5{} Mult if",
         "round ends with your chips",
-        "within {C:attention}5%{} of the {C:attention}Blind{}",
+        "within {C:attention}10%{} of the {C:attention}Blind{}",
         "{C:inactive}(Currently {}{C:mult}+#1#{} {C:inactive}Mult){}",
 	},
 	rarity = 1,
@@ -13,18 +13,32 @@ local jokerInfo = {
 	canEternal = true
 }
 
---[[
+
 function jokerInfo.locDef(self)
-	return { G.GAME.probabilities.normal }
+	return { self.ability.extra.mult }
 end
 
 function jokerInfo.init(self)
-
+	self.ability.extra = {
+		mult = 0,
+		mult_mod = 5
+	}
 end
-]]--
+
 
 function jokerInfo.calculate(self, context)
-	--todo
+	if context.end_of_round and not self.debuff and not context.individual and not context.repetition and not context.blueprint then
+		if G.GAME.chips <= (G.GAME.blind.chips * 1.1) then
+			self.ability.extra.mult = self.ability.extra.mult + self.ability.extra.mult_mod
+			card_eval_status_text(self, 'extra', nil, nil, nil, {message = localize('k_upgrade_ex'), colour = G.C.MULT})
+		end
+	end
+	if context.joker_main and context.cardarea == G.jokers then
+		return {
+			message = localize{type='variable',key='a_mult',vars={self.ability.extra.mult}},
+			mult_mod = self.ability.extra.mult, 
+		}
+	end
 end
 
 
