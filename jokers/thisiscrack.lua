acd67093b3241c80abfa502f799339ed87eaa54f
BarrierTrio <vinylscratch270@gmail.com>
2024-06-09 00:19:07 -0700

    done

--- jokers/thisiscrack.lua
+++ jokers/thisiscrack.lua
@@ -16,18 +16,42 @@ local jokerInfo = {
 }
 
 function jokerInfo.locDef(self)
+	local hand_var = self.ability.extra.crack_hand and localize(self.ability.extra.crack_hand, 'poker_hands') or localize('k_none')
 	return { self.ability.extra.x_mult, self.ability.extra.crack_hand }
 end
 
 function jokerInfo.init(self)
 	self.ability.extra = {
 		x_mult = 1,
-		crack_hand = {"None"} --replace with previous hand?
+		crack_hand = "None" --replace with previous hand?
 	}
 end
 
 function jokerInfo.calculate(self, context)
-	if not context.blueprint then
+	if context.cardarea == G.jokers and context.before and not context.blueprint then
+		local hand = context.scoring_name
+		if hand == self.ability.extra.crack_hand or self.ability.extra.crack_hand == "None" then
+			self.ability.extra.x_mult = self.ability.extra.x_mult + 0.1
+		else
+			self.ability.extra.crack_hand = hand
+			if self.ability.extra.x_mult > 1 then
+                self.ability.extra.x_mult = 1
+                return {
+                    card = self,
+                    message = localize('k_reset')
+                }
+            end
+		end
+		self.ability.extra.crack_hand = hand
+	  end
+	if context.joker_main and context.cardarea == G.jokers then
+		return {
+			message = localize{type='variable',key='a_xmult',vars={self.ability.extra.x_mult}},
+			Xmult_mod = self.ability.extra.x_mult, 
+			--colour = G.C.MULT
+		}
+	end
+	--[[	if not context.blueprint then
 		local reset = true
         --local play_more_than = (G.GAME.hands[context.scoring_name].played or 0)
         for k, v in pairs(G.GAME.hands) do
@@ -48,6 +72,7 @@ function jokerInfo.calculate(self, context)
         end
 		self.ability.extra.crack_hand = context.scoring_name
 	end
+	]]--
 end
 
 
