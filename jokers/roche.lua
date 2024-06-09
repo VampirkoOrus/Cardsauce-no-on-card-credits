1994fee46cdd551a2166b678d181d401d14783b5
BarrierTrio <vinylscratch270@gmail.com>
2024-06-09 02:03:05 -0700

    effect done

--- jokers/roche.lua
+++ jokers/roche.lua
@@ -12,18 +12,48 @@ local jokerInfo = {
 	canEternal = true
 }
 
---[[
+function jokerInfo.tooltip(self, info_queue)
+	info_queue[#info_queue+1] = {key = "guestartist5", set = "Other"}
+end
+
 function jokerInfo.locDef(self)
-	return { G.GAME.probabilities.normal }
+	return { self.ability.gil }
 end
 
 function jokerInfo.init(self)
-
+	self.ability.gil = 13
 end
-]]--
+
 
 function jokerInfo.calculate(self, context)
-	--todo
+	if context.end_of_round and not self.debuff and not context.individual and not context.repetition then
+		if G.GAME.dollars == self.ability.gil then
+			if #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+				local card_type = 'Planet'
+				G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+				G.E_MANAGER:add_event(Event({
+					trigger = 'before',
+					delay = 0.0,
+					func = (function()
+						if G.GAME.current_round.most_played_poker_hand then
+							local _planet = 0
+							for k, v in pairs(G.P_CENTER_POOLS.Planet) do
+								if v.config.hand_type == G.GAME.current_round.most_played_poker_hand then
+									_planet = v.key
+								end
+							end
+							local card = create_card(card_type,G.consumeables, nil, nil, nil, nil, _planet, 'blusl')
+							card:add_to_deck()
+							G.consumeables:emplace(card)
+							G.GAME.consumeable_buffer = 0
+						end
+						return true
+					end)}))
+				card_eval_status_text(self, 'extra', nil, nil, nil, {message = localize('k_plus_planet'), colour = G.C.SECONDARY_SET.Planet})
+				--ret.effect = true
+			end
+		end
+	end
 end
 
 
