54a92f97cf3807ed722a188f1a18eecdae41c6fb
Kekulism <33201764+Kekulism@users.noreply.github.com>
2024-12-28 20:41:21 -0500

    Whoops
    - Fixed Artist credit mistake on Rotten Joker

--- jokers/rotten.lua
+++ jokers/rotten.lua
@@ -10,7 +10,7 @@ local jokerInfo = {
 }
 
 function jokerInfo.loc_vars(self, info_queue, card)
-    info_queue[#info_queue+1] = {key = "guestartist20", set = "Other"}
+    info_queue[#info_queue+1] = {key = "guestartist19", set = "Other"}
 end
 
 function jokerInfo.add_to_deck(self, card)
