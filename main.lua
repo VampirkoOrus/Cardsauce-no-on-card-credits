ad9ff205349abad9ee0c4cad631c4a8a715e54dc
Vivi <viviangiacobbi@gmail.com>
2025-03-25 12:31:16 -0500

    Updated sticker scaling to use SMODS.DrawStep

--- main.lua
+++ main.lua
@@ -879,7 +879,51 @@ SMODS.DrawStep:take_ownership('floating_sprite', {
 	end,
 })
 
+SMODS.DrawStep:take_ownership('stickers', {
+	func = function(self, layer)
+        if self.sticker and G.shared_stickers[self.sticker] then
+			local t, vt = scale_joker_sticker(G.shared_stickers[self.sticker], self)
+            G.shared_stickers[self.sticker].role.draw_major = self
+            G.shared_stickers[self.sticker]:draw_shader('dissolve', nil, nil, nil, self.children.center)
+            G.shared_stickers[self.sticker]:draw_shader('voucher', nil, self.ARGS.send_to_shader, nil, self.children.center)
+			reset_sticker_scale(self, t, vt)
+		elseif (self.sticker_run and G.shared_stickers[self.sticker_run]) and G.SETTINGS.run_stake_stickers then
+            local t, vt = scale_joker_sticker(G.shared_stickers[self.sticker_run], self)
+			G.shared_stickers[self.sticker_run].role.draw_major = self
+            G.shared_stickers[self.sticker_run]:draw_shader('dissolve', nil, nil, nil, self.children.center)
+            G.shared_stickers[self.sticker_run]:draw_shader('voucher', nil, self.ARGS.send_to_shader, nil, self.children.center)
+			reset_sticker_scale(self, t, vt)
+		end
+
+        for k, v in pairs(SMODS.Stickers) do
+            if self.ability[v.key] then
+                if v and v.draw and type(v.draw) == 'function' then
+                    v:draw(self, layer)
+                else
+					local t, vt = scale_joker_sticker(G.shared_stickers[v.key], self)
+                    G.shared_stickers[v.key].role.draw_major = self
+                    G.shared_stickers[v.key]:draw_shader('dissolve', nil, nil, nil, self.children.center)
+                    G.shared_stickers[v.key]:draw_shader('voucher', nil, self.ARGS.send_to_shader, nil, self.children.center)
+					reset_sticker_scale(self, t, vt)
+				end
+            end
+        end
+    end,
+})
+
 local old_draw_step_db = SMODS.DrawSteps.debuff.func
+SMODS.DrawStep:take_ownership('debuff', {
+	func = function(self, layer)
+		old_draw_step_db(self,layer)
+		if self.cured_debuff then
+			self.children.center:draw_shader('debuff', nil, self.ARGS.send_to_shader)
+			if self.children.front and self.ability.effect ~= 'Stone Card' and not self.config.center.replace_base_card then
+				self.children.front:draw_shader('debuff', nil, self.ARGS.send_to_shader)
+			end
+		end
+	end,
+})
+
 SMODS.DrawStep:take_ownership('debuff', {
 	func = function(self, layer)
 		old_draw_step_db(self,layer)
