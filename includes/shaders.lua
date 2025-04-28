7c7933fd73fca06ee4885073c8351d8526ac0005
Vivi <viviangiacobbi@gmail.com>
2025-04-27 20:34:43 -0500

    Take three

--- includes/shaders.lua
+++ includes/shaders.lua
@@ -1,7 +1,6 @@
-local old_draw_step_fs = SMODS.DrawSteps.floating_sprite.func
 SMODS.DrawStep:take_ownership('floating_sprite', { 
 	func = function(self, layer)
-        if self.config.center.soul_pos and not self.ability.set == 'csau_Stand' and (self.config.center.discovered or self.bypass_discovery_center) then
+        if self.config.center.soul_pos and self.ability.set ~= 'csau_Stand' and (self.config.center.discovered or self.bypass_discovery_center) then
             local scale_mod = 0.07 + 0.02*math.sin(1.8*G.TIMERS.REAL) + 0.00*math.sin((G.TIMERS.REAL - math.floor(G.TIMERS.REAL))*math.pi*14)*(1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL)))^3
             local rotate_mod = 0.05*math.sin(1.219*G.TIMERS.REAL) + 0.00*math.sin((G.TIMERS.REAL)*math.pi*5)*(1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL)))^2
 
@@ -11,6 +10,9 @@ SMODS.DrawStep:take_ownership('floating_sprite', {
                 self.hover_tilt = self.hover_tilt*1.5
                 self.children.floating_sprite:draw_shader('hologram', nil, self.ARGS.send_to_shader, nil, self.children.center, 2*scale_mod, 2*rotate_mod)
                 self.hover_tilt = self.hover_tilt/1.5
+            else
+                self.children.floating_sprite:draw_shader('dissolve',0, nil, nil, self.children.center,scale_mod, rotate_mod,nil, 0.1 + 0.03*math.sin(1.8*G.TIMERS.REAL),nil, 0.6)
+                self.children.floating_sprite:draw_shader('dissolve', nil, nil, nil, self.children.center, scale_mod, rotate_mod)
             end
 
             if self.edition then
