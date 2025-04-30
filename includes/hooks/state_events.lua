f6fe96b1622a3bbc43c054c01cdc1b80c47f0842
Kekulism <33201764+Kekulism@users.noreply.github.com>
2025-04-30 13:58:42 -0400

    bugfix 65
    - Idol now resets properly
    - Black Spine Junka now shows its x_mult properly

--- includes/hooks/state_events.lua
+++ includes/hooks/state_events.lua
@@ -18,3 +18,27 @@ function G.FUNCS.draw_from_deck_to_hand(self, e)
 		end
 	end
 end
+
+local ric_ref = reset_idol_card
+function reset_idol_card()
+	if G.GAME and G.GAME.wigsaw_suit then
+		G.GAME.current_round.idol_card.rank = 'Ace'
+		G.GAME.current_round.idol_card.suit = 'Spades'
+		local valid_idol_cards = {}
+		for k, v in ipairs(G.playing_cards) do
+			if v.ability.effect ~= 'Stone Card' then
+				if not SMODS.has_no_suit(v) and not SMODS.has_no_rank(v) and v:is_suit(G.GAME.wigsaw_suit) then
+					valid_idol_cards[#valid_idol_cards+1] = v
+				end
+			end
+		end
+		if valid_idol_cards[1] then
+			local idol_card = pseudorandom_element(valid_idol_cards, pseudoseed('idol'..G.GAME.round_resets.ante))
+			G.GAME.current_round.idol_card.rank = idol_card.base.value
+			G.GAME.current_round.idol_card.suit = idol_card.base.suit
+			G.GAME.current_round.idol_card.id = idol_card.base.id
+		end
+	else
+		ric_ref()
+	end
+end
--- items/jokers/junka.lua
+++ items/jokers/junka.lua
@@ -30,7 +30,7 @@ local jokerInfo = {
 
 function jokerInfo.loc_vars(self, info_queue, card)
     info_queue[#info_queue+1] = {key = "csau_artistcredit", set = "Other", vars = { G.csau_team.yunkie } }
-    return { vars = { card.ability.extra.mult_mod, card.ability.extra.prob_mod, G.FUNCS.csau_add_chance(card.ability.extra.prob_extra, true), card.ability.extra.prob, card.ability.extra.mult } }
+    return { vars = { card.ability.extra.x_mult_mod, card.ability.extra.prob_mod, G.FUNCS.csau_add_chance(card.ability.extra.prob_extra, true), card.ability.extra.prob, card.ability.extra.x_mult } }
 end
 
 function jokerInfo.locked_loc_vars(self, info_queue, card)
