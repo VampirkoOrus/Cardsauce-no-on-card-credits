fcf7586ad4ccaf76560a69ea36fec34f2f482811
Kekulism <33201764+Kekulism@users.noreply.github.com>
2025-05-09 15:49:27 -0400

    whoops 2

--- items/jokers/businesstrading.lua
+++ items/jokers/businesstrading.lua
@@ -12,13 +12,11 @@ local jokerInfo = {
     blueprint_compat = false,
     eternal_compat = true,
     perishable_compat = true,
-    width = 178,
-    height = 238,
     streamer = "vinny",
 }
 
 function jokerInfo.loc_vars(self, info_queue, card)
-    info_queue[#info_queue+1] = {key = "csau_artistcredit", set = "Other", vars = { G.csau_team.bard } }
+    info_queue[#info_queue+1] = {key = "csau_artistcredit", set = "Other", vars = { G.csau_team.keku } }
     return { vars = {card.ability.extra.dollars, G.GAME.probabilities.normal, card.ability.extra.destroy} }
 end
 
